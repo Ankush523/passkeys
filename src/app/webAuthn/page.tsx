5c36e2a71be61b762368fe7aa2a4214ee996a0af
Ankush523 <ankush.ad050203@gmail.com>
2023-09-18 00:53:17 +0530

    wallet addr generated

--- src/app/webAuthn/page.tsx
+++ src/app/webAuthn/page.tsx
@@ -3,13 +3,31 @@ import { FC, useCallback, useEffect, useState } from "react";
 import { client, parsers } from "@passwordless-id/webauthn";
 import { AuthenticationParsed } from "@passwordless-id/webauthn/dist/esm/types";
 import styles from "./webAuthn.module.css";
+import Wallet from 'ethereumjs-wallet';
 import "../globals.css";
 export const WebAuthPage: FC = () => {
+
   const [username, setUsername] = useState<string>(
     window.localStorage.getItem("username") || ""
   );
 
+  const [walletAddress, setWalletAddress] = useState<string | null>(
+    window.localStorage.getItem("wallet_" + username) || null
+  );
+  
+
   const isClientAvailable = client.isAvailable();
+
+  // Wallet generation
+
+  const generateEthereumWallet = () => {
+    const wallet = Wallet.generate();
+    return {
+      address: `0x${wallet.getAddress().toString('hex')}`,
+      privateKey: wallet.getPrivateKeyString(),
+    };
+  }
+  
 
   // Registration
 
@@ -17,7 +35,6 @@ export const WebAuthPage: FC = () => {
   const challenge =
     window.localStorage.getItem("challenge_" + username) ||
     window.crypto.randomUUID();
-
   const checkIsRegistered = useCallback(async () => {
     setIsRegistered(!!window.localStorage.getItem("credential_" + username));
   }, [username]);
@@ -27,6 +44,7 @@ export const WebAuthPage: FC = () => {
       checkIsRegistered();
     }
   }, [checkIsRegistered, username]);
+  
 
   const register = useCallback(async () => {
     const res = await client.register(username, challenge, {
@@ -36,6 +54,15 @@ export const WebAuthPage: FC = () => {
       attestation: false,
       debug: false,
     });
+    console.log(res);
+
+    if (res) {
+      // Generate Ethereum wallet and store it in localStorage
+      const { address, privateKey } = generateEthereumWallet();
+      window.localStorage.setItem("walletAddress_" + username, address);
+      window.localStorage.setItem("walletPrivateKey_" + username, privateKey);
+    }
+
     const parsed = parsers.parseRegistration(res);
     window.localStorage.setItem("username", username);
     window.localStorage.setItem("credential_" + username, parsed.credential.id);
@@ -62,6 +89,7 @@ export const WebAuthPage: FC = () => {
       if (!username) {
         credentials = getAllStoredCredentials();
       }
+    
       const res = await client.authenticate(
         credentials,
         challenge,
@@ -71,19 +99,52 @@ export const WebAuthPage: FC = () => {
           timeout: 60000,
         }
       );
+    
       const parsed = parsers.parseAuthentication(res);
+    
+      // If we had no username, try to deduce it from the authenticated credential.
       if (!username) {
-        // If we had no username, try to deduce it from the authenticated credential.
         const usernameKey = Object.keys(window.localStorage).find(
           key => window.localStorage.getItem(key) === parsed.credentialId
         );
         if (usernameKey) {
-          setUsername(usernameKey.replace("credential_", ""));
+          const deducedUsername = usernameKey.replace("credential_", "");
+          setUsername(deducedUsername); 
+    
+          // Fetch the wallet address using the newly deduced username
+          const address = window.localStorage.getItem("walletAddress_" + deducedUsername);
+          if (address) {
+            setWalletAddress(address);
+          }
+        }
+      } else {
+        // If we already have a username, fetch the wallet address.
+        const address = window.localStorage.getItem("walletAddress_" + username);
+        if (address) {
+          setWalletAddress(address);
         }
       }
+    
       setIsAuthenticated(true);
       setAuthenticationData(parsed);
-  }, [challenge, username]);
+    }, [challenge, username]);
+    
+
+  //Sign out
+  const handleSignOut = () => {
+    // Clear user session and state
+    setUsername("");
+    setIsAuthenticated(false);
+    setAuthenticationData(null);
+    setWalletAddress(null);
+  
+    // Optionally, you can also clear local storage if needed
+    // window.localStorage.clear(); 
+    // OR selectively:
+    // window.localStorage.removeItem("username");
+    // ... remove any other items as necessary ...
+  };
+  
   
 
   return (
@@ -123,6 +184,19 @@ export const WebAuthPage: FC = () => {
           Login
         </button>
       </p>
+
+      {walletAddress && (
+      <p className="row-between">
+        <b className={styles.text}>Wallet Address:</b>{" "}
+        <span>{walletAddress}</span>
+      </p>
+    )}
+
+    {isAuthenticated && (
+      <button className={styles.button} onClick={handleSignOut}>
+        Sign Out
+      </button>
+    )}
 
       {authenticationData && (
         <>
